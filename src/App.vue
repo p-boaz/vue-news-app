01d91b5b66b9deceefa3b468a851d1f94a618355
Michael Wanyoike <micwanyoike@gmail.com>
2021-02-15 23:40:01 +0300

    - Implemented Loading message - Implemented Error messages - Fixed missing image issue

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,23 @@
       News Section : <span class="text-green-700">{{ section }}</span>
     </h2>
     <NewsFilter v-model="section" :fetch="fetchNews" />
-    <NewsList :posts="posts" />
+    <NewsList v-if="!loading && !error" :posts="posts" />
+    <!-- Start of loading animation -->
+    <div class="mt-40" v-if="loading">
+      <p class="text-6xl font-bold text-center text-gray-500 animate-pulse">
+        Loading...
+      </p>
+    </div>
+    <!-- End of loading animation -->
+
+    <!-- Start of error alert -->
+    <div class="mt-12 bg-red-50" v-if="error">
+      <h3 class="px-4 py-1 text-4xl font-bold text-white bg-red-800">
+        {{ this.error.title }}
+      </h3>
+      <p class="p-4 text-lg font-bold text-red-900">{{ this.error.message }}</p>
+    </div>
+    <!-- End of error alert -->
   </Layout>
 </template>
 
@@ -26,17 +42,28 @@ export default {
     return {
       section: "home",
       posts: [],
+      loading: false,
+      error: null,
     }
   },
   methods: {
     extractImage(post) {
+      const defaultImg = {
+        url: "http://placehold.it/210x140?text=N/A",
+        caption: post.title,
+      }
+      if (!post.multimedia) {
+        return defaultImg
+      }
       let imgObj = post.multimedia.find(
         media => media.format === "mediumThreeByTwo210"
       )
-      return imgObj
+      return imgObj ? imgObj : defaultImg
     },
     async fetchNews() {
       try {
+        this.error = null
+        this.loading = true
         const url = `https://api.nytimes.com/svc/topstories/v2/${this.section}.json?api-key=${api}`
         const response = await axios.get(url)
         const results = response.data.results
@@ -52,14 +79,25 @@ export default {
       } catch (err) {
         if (err.response) {
           // client received an error response (5xx, 4xx)
-          console.log("Server Error:", err)
+          this.error = {
+            title: "Server Response",
+            message: err.message,
+          }
         } else if (err.request) {
           // client never received a response, or request never left
-          console.log("Network Error:", err)
+          this.error = {
+            title: "Unable to Reach Server",
+            message: err.message,
+          }
         } else {
-          console.log("Client Error:", err)
+          // There's probably an error in your code
+          this.error = {
+            title: "Application Error",
+            message: err.message,
+          }
         }
       }
+      this.loading = false
     },
   },
   mounted() {
